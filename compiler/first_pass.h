ab347432d7221f2dd22c9b9867120c104b15c268
Alonso <alon8729@gmail.com>
2023-07-24 11:35:48 +0300

    added extern symbol ctor

---
+++ compiler/first_pass.h
@@ -0,0 +1,11 @@
+#include "assembler.h"
+
+
+/**
+ * @brief 
+ * 
+ * @param am_file 
+ * @param obj 
+ * @return obj_file return obj file if compilation succeeded,otherwise return NULL 
+ */
+obj_file first_pass(FILE *am_file, obj_file obj);
