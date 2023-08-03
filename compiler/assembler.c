2b5fa5c4f1a229def2462ad5e096a6533acb18ae
Alonso <alon8729@gmail.com>
2023-08-03 14:27:08 +0300

    Removed functions and structures
    functions and data structures have been removed to common_structs dir

--- compiler/assembler.c
+++ compiler/assembler.c
@@ -1,100 +1,9 @@
 #include "assembler.h"
 #include "first_pass.h"
 #include "second_pass.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-struct symbol *create_symbol(char sym_name[MAX_LABEL_LEN + 1], short address,
-                             int symbol_type, int declared_line) {
-  struct symbol *new_symbol = malloc(sizeof(struct symbol));
-  if (new_symbol) {
-    strcpy(new_symbol->symbol_name, sym_name);
-    new_symbol->symbol_types = symbol_type;
-    new_symbol->declared_line = declared_line;
-    new_symbol->address = address;
-    return new_symbol;
-  }
-  printf("Memory allocation failed\n");
-  exit(1);
-}
-
-struct extern_symbol *create_extern_symbol(char sym_name[MAX_LABEL_LEN + 1],
-                                           short address) {
-  extern_symbol *new_ext = malloc(sizeof(extern_symbol));
-  if (new_ext) {
-    strcpy(new_ext->extern_name, sym_name);
-    new_ext->call_address = create_dynamic_array(sizeof(short));
-    insert_item(new_ext->call_address, &address);
-    return new_ext;
-  }
-  printf("Memory allocation failed\n");
-  exit(1);
-}
-
-machine_word *create_machine_word(short binary_code) {
-  machine_word *word = (machine_word *)malloc(sizeof(machine_word));
-  if (!word) {
-    printf("Memory allocation failed\n");
-    exit(1);
-  }
-  word->binary_code = binary_code;
-  return word;
-}
-
-obj_file create_obj_file() {
-  obj_file new_obj = malloc(sizeof(struct object_file));
-  new_obj->symbol_table = create_dynamic_array(sizeof(struct symbol));
-  new_obj->symbol_search = NULL;
-  new_obj->symbol_search = create_trie();
-  new_obj->code_image = create_dynamic_array(sizeof(machine_word));
-  new_obj->data_image = create_dynamic_array(sizeof(machine_word));
-  new_obj->extern_table = create_dynamic_array(sizeof(struct extern_table));
-  return new_obj;
-}
-
-void destroy_machine_word(obj_file obj) {
-  if (obj) {
-    int i;
-    int data_image_size = get_item_count(obj->data_image);
-    int code_image_size = get_item_count(obj->code_image);
-    for (i = 0; i < code_image_size; i++) {
-      machine_word *m = get_item(obj->code_image, i);
-      free(m);
-    }
-    for (i = 0; i < data_image_size; i++) {
-      machine_word *m = get_item(obj->data_image, i);
-      free(m);
-    }
-  }
-}
-
-void destroy_extern_table(d_arr *ext_table) {
-  if (ext_table) {
-    int extern_table_size = get_item_count(*ext_table);
-    int i;
-    for (i = 0; i < extern_table_size; i++) {
-      struct extern_table *curr_ext = get_item(*ext_table, i);
-      if (curr_ext) {
-        destroy_dynamic_array(curr_ext->call_address);
-        free(curr_ext);
-      }
-    }
-    destroy_dynamic_array(*ext_table);
-  }
-}
-
-void free_obj_file(obj_file obj) {
-  destroy_dynamic_array(obj->symbol_table);
-  trie_destroy(obj->symbol_search);
-  destroy_dynamic_array(obj->code_image);
-  destroy_dynamic_array(obj->data_image);
-  destroy_extern_table(&obj->extern_table);
-  free(obj);
-}
 
 /**
- * @brief
+ * @brief check if all the entries that has been declared was used
  *
  * @param obj
  * @return int return 0 if there is no error, otherwise return 1
@@ -125,6 +34,8 @@ static int check_if_memory_exceeded(int total_m_words) {
   return total_m_words > MAX_MEMORY ? 1 : 0;
 }
 
+
+
 int compile_program(int file_count, char **file_names) {
   FILE *am_file;
   const char *am_file_name;
@@ -134,7 +45,7 @@ int compile_program(int file_count, char **file_names) {
   int memory_check;
 
   for (i = 0; i < file_count; i++) {
-    am_file_name = preprocess(*(file_names + i));
+    am_file_name = preprocess(file_names[i]);
     if (am_file_name) {
       am_file = fopen(am_file_name, "r");
       if (am_file) {
@@ -151,7 +62,7 @@ int compile_program(int file_count, char **file_names) {
                                   limit, and all the entries has been defined*/
             rewind(am_file);
             if (second_pass(curr_obj, am_file)) {
-              /*send to output files function*/
+              output_all_files(curr_obj, file_names[i]);
             }
             fclose(am_file);
             free_obj_file(curr_obj);
--- compiler/assembler.h
+++ compiler/assembler.h
@@ -1,142 +1,24 @@
 #ifndef __ASSEMBLER_H_
 #define __ASSEMBLER_H_
-#include "../preprocessor/preprocessor.h"
-#include "/mnt/d/Documents/VSC Projects/mmn14/lexer.h"
-#include "../data_structures/table/table.h"
+#include "../data_structures/common_structs/common.h"
+#include "../data_structures/dynamic_array/dynamic_array.h"
 #include "../data_structures/trie/trie.h"
+#include "../line_structure/lineToStructure.h"
+#include "../preprocessor/preprocessor.h"
+#include "../output_files/output_files.h"
 #define OP_MAX_NUM 2
 #define BASE_ADDRESS 100
 #define MAX_MEMORY 1024
 #define MACHINE_WORD_SIZE 12
-/**
- * @brief 
- * 
- */
-struct object_file{
-    d_arr symbol_table;
-    Trie symbol_search;
-    d_arr code_image;
-    d_arr data_image;
-    d_arr extern_table;
-};
 
-typedef struct object_file* obj_file;
-
-struct symbol{
-    char symbol_name[MAX_NAME_SIZE];
-    short address;
-    unsigned short declared_line;
-    enum{
-        entry,
-        external,
-        code,
-        data,
-        entry_code,
-        entry_data
-    }symbol_types;
-};
-
-/*symbols that have been declared before being defined*/
-
-
-struct extern_table{
-    char extern_name[MAX_NAME_SIZE];
-    d_arr call_address;
-};
-
-typedef struct extern_symbol{
-    char extern_name[MAX_NAME_SIZE];
-    d_arr call_address;
-}extern_symbol;  
-
-typedef struct machine_word{
-    short binary_code;
-} machine_word;
-
-/**
- * @brief Create a symbol object
- * 
- * @param sym_name 
- * @param address 
- * @param symbol_type 
- * @param declared_line 
- * @return struct symbol* 
- */
-struct symbol* create_symbol(char sym_name[MAX_NAME_SIZE], short address ,int symbol_type, int declared_line);
-
-/**
- * @brief Create a machine word object
- * 
- * @param binary_code 
- * @return machine_word* 
- */
-machine_word* create_machine_word(short binary_code);
-/**
- * @brief 
- * 
- * @param file_count 
- * @param file_names 
- * @return object_file 
- */
-
-/**
- * @brief Create a extern symbol object
- * 
- * @param sym_name 
- * @param address 
- * @return struct extern_symbol* 
- */
- struct extern_symbol* create_extern_symbol(char sym_name[MAX_NAME_SIZE],short address);
-
-/**
- * @brief Create an object file object
- * 
- * @return struct object_file 
- */
-obj_file create_obj_file();
-
-/**
- * @brief 
- * 
- */
-
-/**
- * @brief 
- * 
- * @param ext_table 
- */
-void destroy_extern_table(d_arr* ext_table);
-
-/**
- * @brief deallocate the memory of all the data structures inside the object
- * 
- */
-void free_obj_file(obj_file);
 
 /**
  * @brief 
  * 
  * @param file_count 
  * @param file_names 
- * @return struct object_file 
+ * @return int 
  */
-int compile_program(int file_count,char** file_names);
-
-/**
- * @brief 
- * 
- * @param obj 
- * @return int return 0 if there is no error, otherwise return 1
- */
-static int check_entry_definition(obj_file obj);
-
-/**
- * @brief 
- * 
- * @param total_m_words machine words counter
- * @return int return 1 if memory exceeded, otherwise return 0
- */
-static int check_if_memory_exceeded(int total_m_words);
-
+int compile_program(int file_count, char **file_names);
 
 #endif
