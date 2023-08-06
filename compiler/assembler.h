9a04ba32e0e3dc9df90ba9e68606a896abac9e33
Alonso <alon8729@gmail.com>
2023-08-06 12:52:01 +0300

    changed include filename

--- compiler/assembler.h
+++ compiler/assembler.h
@@ -3,7 +3,7 @@
 #include "../data_structures/common_structs/common.h"
 #include "../data_structures/dynamic_array/dynamic_array.h"
 #include "../data_structures/trie/trie.h"
-#include "../line_structure/lineToStructure.h"
+#include "../line_structure/line_to_structure.h"
 #include "../preprocessor/preprocessor.h"
 #include "../output_files/output_files.h"
 #define OP_MAX_NUM 2
@@ -11,6 +11,14 @@
 #define MAX_MEMORY 1024
 #define MACHINE_WORD_SIZE 12
 
+/**
+ * @brief 
+ * 
+ * @param obj 
+ * @param address 
+ * @param extern_name 
+ */
+void add_to_extern_table(obj_file *obj, int address, char *extern_name);
 
 /**
  * @brief 
