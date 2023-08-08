f9af7af5abdf233abb8c0d06cdc9e2117a6a0381
Alonso <alon8729@gmail.com>
2023-08-08 08:30:37 +0300

    Added function
    Add extern symbol to extern table

--- compiler/assembler.c
+++ compiler/assembler.c
@@ -12,12 +12,14 @@ static int check_entry_definition(obj_file obj) {
   int error = 0;
   int i;
   struct symbol *sym;
-  int size = obj->symbol_table->item_count;
+  int size;
+  size = obj->symbol_table->item_count;
   for (i = 0; i < size; i++) {
     sym = get_item(obj->symbol_table, i);
     if (sym->symbol_types == entry) {
       error = 1;
-      printf("Entry label %s was declared in line %d but was never defined\n",
+      printf("Entry label %s was declared in line %d as entry but was never "
+             "defined\n",
              sym->symbol_name, sym->declared_line);
     }
   }
@@ -34,7 +36,30 @@ static int check_if_memory_exceeded(int total_m_words) {
   return total_m_words > MAX_MEMORY ? 1 : 0;
 }
 
+void add_to_extern_table(obj_file *obj, int address, char *extern_name) {
+  int i;
+  obj_file curr_obj;
+  int table_size;
+  extern_symbol *new_ext;
+  struct extern_table *curr_ext;
+  curr_obj = *obj;
+  table_size = get_item_count(curr_obj->extern_table);
 
+  /*search for extern name in the extern table*/
+  for (i = 0; i < table_size; i++) {
+    curr_ext = get_item(curr_obj->extern_table, i);
+    if (strcmp(extern_name, curr_ext->extern_name) == 0) {
+      insert_item(curr_ext->call_address, &address);
+      return;
+    }
+  }
+  /*adding a new item to the extern table*/
+  new_ext = create_extern_symbol(extern_name, address);
+  new_ext->call_address = create_dynamic_array(sizeof(int));
+  insert_item(new_ext->call_address, &address);
+  insert_item(curr_obj->extern_table, new_ext);
+  return;
+}
 
 int compile_program(int file_count, char **file_names) {
   FILE *am_file;
